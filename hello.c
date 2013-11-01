70c09dbc4fc61486ab87e0d88c47858fb0cb078c
harshavardhangelivi <harshavardhangelivi@gmail.com>
2013-11-02 00:06:01 +0530

    with source files

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+static int __init hello_init(void){
+    printk(KERN_INFO "Hello world!\n");
+    return 0;
+}
+
+static void __exit hello_cleanup(void){
+    printk(KERN_INFO "Module removed.Goodbye\n");
+}
+
+module_init(hello_init);
+module_exit(hello_cleanup);
