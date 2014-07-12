e4a0db8bbf574ce0b7207b3003500d90eb49ec4e
Gelivi Harsha Vardhan <harshavardhangelivi@gmail.com>
2014-07-13 00:52:45 +0530

    Update fops.h

--- tagfs/fops.h
+++ tagfs/fops.h
@@ -1,5 +1,4 @@
 static int tfs_dirops_iterate (struct file * filp, struct dir_context * dcxt){
-	static int checked_child=0;
 	loff_t pos = dcxt->pos;
 	struct inode *inode = file_inode(filp);
 	struct super_block *sb = inode->i_sb;
@@ -7,7 +6,7 @@ static int tfs_dirops_iterate (struct file * filp, struct dir_context * dcxt){
 	struct tfs_dir_entry *entry;
 	struct tfs_inode_mem *inode_mem;
 	int children, i;
-
+	if(pos) return 0;
 	inode_mem = inode->i_private;
 	if(!S_ISDIR(inode->i_mode)){
 		printk(KERN_ERR"not a directry\n");
@@ -20,11 +19,6 @@ static int tfs_dirops_iterate (struct file * filp, struct dir_context * dcxt){
 	};
 	entry = (struct tfs_dir_entry *)bh->b_data;
 	children = inode_mem->i_child_count;
-	if(checked_child == children){
-		brelse(bh);
-		checked_child=0;
-		return 0;
-	}
 	for(i=0;i<children;i++){
 		dir_emit(dcxt, entry->name, TFS_FILE_MAXLEN, entry->ino, DT_UNKNOWN);
 		dcxt->pos += sizeof(struct tfs_dir_entry);
